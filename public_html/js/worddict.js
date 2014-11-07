a923416317326d49aae7d48c09fc47d4d18eac34
dave00 <dave00ster@gmail.com>
2014-11-07 17:08:57 +0100

    refactoring

---
+++ public_html/js/worddict.js
@@ -0,0 +1,14 @@
+worddict = {
+    words: {
+        'dog': 'kutya',
+        'cat': 'macska'
+        //--BEGIND-WORD-LISTING
+        //--END-WORD-LISTING
+    },
+    initialize: function () {
+         
+    },
+    getWordForKey: function (key) {
+        return this.words[key];
+    }
+}
