dae5c823cec1d6be2e8a7f7e5638906173e7aa0d
dave00 <dave00ster@gmail.com>
2014-11-09 20:38:38 +0100

    word match feature in game module

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -1,23 +1,27 @@
 game = {
-    originalword : null,
-    translatedword : null,
-    guesses : [],
-    newGame : function () {
+    originalword: null,
+    translatedword: null,
+    guesses: null,
+    newGame: function () {
         this.originalword = worddict.getNewWord();
         this.translatedword = worddict.getWordForKey(this.originalword);
+        this.guesses = [];
     },
-    addCharacter : function (c) {
+    addCharacter: function (c) {
         if (this.guesses.length >= this.translatedword.length) {
             return false;
         }
         this.guesses.push(c);
         return true;
     },
-    removeCharacter : function() {
+    removeCharacter: function () {
         if (this.guesses.length == 0) {
             return false;
         }
         this.guesses.pop();
         return true;
+    },
+    matches : function () {
+        return this.translatedword == this.guesses.join('');
     }
 };
--- test/gametest.js
+++ test/gametest.js
@@ -1,26 +1,52 @@
-describe("Game suite", function() {
+describe("Game suite", function () {
 
-  it("should only be able to add as many characters as many characters are in the word", function() {
+    it("should only be able to add as many characters as many characters are in the word", function () {
 
-    game.newGame();
+        game.newGame();
 
-    var lengthOfWord = game.translatedword.length;
-    for (var i = 0; i<lengthOfWord; i++) {
+        var lengthOfWord = game.translatedword.length;
+        for (var i = 0; i < lengthOfWord; i++) {
+            expect(game.addCharacter('a')).toBe(true);
+        }
+
+        // out of space
+        expect(game.addCharacter('a')).toBe(false);
+
+        // delete one character
+        expect(game.removeCharacter()).toBe(true);
+
+        // can add one again
         expect(game.addCharacter('a')).toBe(true);
-    }
 
-    // out of space
-    expect(game.addCharacter('a')).toBe(false);
+        // then out of space again
+        expect(game.addCharacter('a')).toBe(false);
 
-    // delete one character
-    expect(game.removeCharacter()).toBe(true);
+    });
 
-    // can add one again
-    expect(game.addCharacter('a')).toBe(true);
+    it("should match correct guess", function () {
 
-    // then out of space again
-    expect(game.addCharacter('a')).toBe(false);
+        game.newGame();
 
-  });
+        var lengthOfWord = game.translatedword.length;
+        for (var i = 0; i < lengthOfWord; i++) {
+            game.addCharacter(game.translatedword[i]);
+        }
+
+        expect(game.matches()).toBe(true);
+
+    });
+
+    it("should not match in correct guess", function () {
+
+        game.newGame();
+
+        var lengthOfWord = game.translatedword.length;
+        for (var i = 0; i < lengthOfWord; i++) {
+            game.addCharacter('b');
+        }
+
+        expect(game.matches()).toBe(false);
+
+    });
 
 });
