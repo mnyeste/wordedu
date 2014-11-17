b44938f0857f06e70e4fbc8d866cc8d8242ecc67
dave00 <dave00ster@gmail.com>
2014-11-17 18:37:25 +0100

    v0.3.0 loading dictionary with ajax

--- etc/tools/addwords.py
+++ etc/tools/addwords.py
@@ -1,7 +1,7 @@
 import codecs
 
 def log(text):
-	print "[addwords] -",text
+    print "[addwords] -",text
 
 log("read lines from words.txt")
 
@@ -13,40 +13,21 @@ log("split lines")
 
 splittedLines = []
 for line in lines:
-	splittedLine = line.split(',')
-        if not "#" in line:
-            splittedLines.append(splittedLine)
+    splittedLine = line.split(',')
+    if not "#" in line:
+        splittedLines.append(splittedLine)
 
-log("read dict file")
+f = codecs.open('../../public_html/js/words.json', 'w')
 
-f = codecs.open('../../public_html/js/worddict.js', 'r')
-dictlines = f.readlines()
-f.close()
+f.write("{\n")
 
-state = 0;
+i = 0
+for splittedLine in splittedLines:
+    f.write("\"{0}\":\"{1}\"".format(splittedLine[0],splittedLine[1]));
+    if (i < len(splittedLines) - 1):
+        f.write(",")
+    f.write("\n")
+    i = i + 1
 
-f = codecs.open('../../public_html/js/worddict.js', 'w')
-for dictline in dictlines:
-	if "BEGIND-WORD-LISTING" in dictline:
-		state = 1
-	elif "END-WORD-LISTING" in dictline:
-		state = 3
-
-	if state == 0:
-		f.write(dictline)
-		pass
-	elif state == 1:
-		state = 2
-		f.write(dictline)
-		i = 0
-		for splittedLine in splittedLines:
-			f.write("        '{0}':'{1}'".format(splittedLine[0],splittedLine[1]));
-			if (i < len(splittedLines) - 1):
-				f.write(",")
-			f.write("\n")
-			i = i + 1
-	elif state == 3:
-		f.write(dictline)
-		pass
-
+f.write("}\n")
 f.close()
--- public_html/js/worddict.js
+++ public_html/js/worddict.js
@@ -1,72 +1,17 @@
-worddict = {
-    words: {
-        //--BEGIND-WORD-LISTING
-        'source':'forrás',
-        'in some cases':'néhány esetben',
-        'generate':'létrehoz',
-        'occure':'előfordul',
-        'corporate':'együttműködik',
-        'remain':'marad',
-        'maintain':'fenntart',
-        'prove':'bizonyít',
-        'resist':'ellenáll',
-        'resistant':'ellenáll',
-        'on the basis of personal taste':'egyéni ízlés alapján',
-        'define':'meghatároz',
-        'musty':'dohos',
-        'stale':'állott',
-        'rotten':'rohadt',
-        'rancid':'avas',
-        'sensory characteristic':'érzékszervi tulajdonságok',
-        'remain intact':'sértetlen marad',
-        'make it possible':'lehetővé tesz',
-        'vary':'változik',
-        'variant':'változat',
-        'version':'változat',
-        'procedure':'eljárs',
-        'shred':'aprít',
-        'mill':'őröl',
-        'immiscible':'nem vegyíthető',
-        'dissolv':'feloldódik',
-        'among':'között',
-        'take':'igénybevesz',
-        'take':'tart valameddig',
-        'late':'tart valameddig',
-        'certain amount of time':'bizonyos ideig',
-        'vapor pressure':'gőznyomás',
-        'multi-stage batch extraction':'többfokozatú szakaszos extrakció',
-        'continous counter-current extraction':'folyamatos ellenáramú extakció',
-        'continous direct-current extraction':'folymatos egyenáramú extrakció',
-        'sequence':'sorrend',
-        'consistent':'egyenletes',
-        'equable':'egyenletes',
-        'equal':'egyenlő',
-        'equalent':'egyenértékű',
-        'proven':'igazolt',
-        'apparent':'látszólagos',
-        'due to':'miatt',
-        'essential':'nélkülözhetetlen',
-        'interact with':'kölcsönhtásban van',
-        'multiple':'többszörös',
-        'separate':'elkülönít',
-        'cultivate':'termel',
-        'cultivated':'művelt',
-        'ripe':'érett',
-        'ripeness':'érettség',
-        'ripen':'érik',
-        'from a botanical point of view':'növénytani szempontból nézve',
-        'firmly':'határozottan',
-        'break up':'feloszt',
-        'soluble':'oldódó',
-        'solution':'oldat',
-        'solvent':'oldószer',
-        'means':'eszközök',
-        'measures':'intézkedések',
-        'a distinction is made':'különbséget kell tenni'
-        //--END-WORD-LISTING
-    },
+var worddict = {
+    words: {},
     initialize: function () {
-         
+        $.ajax({
+            url: 'js/words.json',
+            async: false,
+            dataType: 'json',
+            success: function (response) {
+                $.each(response, function (key, val) {
+                    worddict.words[key] = val;
+                });
+                wordedulogger.debug("worddict initialized with " + Object.keys(worddict.words).length + " words");
+            }
+        });
     },
     getWordForKey: function (key) {
         return this.words[key];
